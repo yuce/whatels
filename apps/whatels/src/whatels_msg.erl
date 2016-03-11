c6180fd38168bb233b7e7cd681a3f25f748bdebd
Yuce Tekol <yucetekol@gmail.com>
2016-03-11 20:01:40 +0200

    using yes_msg to decode messages

--- apps/whatels/src/whatels_msg.erl
+++ apps/whatels/src/whatels_msg.erl
@@ -33,8 +33,6 @@
 -export([encode/1,
          decode/1]).
 
--define(NL, <<"\r\n">>).
-
 %% whatels msg:
 %% OPNAME PayloadSize\r\n
 %% Payload\r\n
@@ -44,77 +42,37 @@
 encode({symbols, Path, Symbols}) when is_binary(Path) ->
     BinSymbols = jsx:encode(Symbols),
     Payload = <<Path/binary, "\r\n", BinSymbols/binary>>,
-    encode_msg(<<"path-symbols">>, Payload);
+    yes_msg:encode(<<"path-symbols">>, Payload);
 
 encode({symbolsQ, Path}) ->
-    encode_msg(<<"path-symbols?">>, Path);
+    yes_msg:encode(<<"path-symbols?">>, Path);
 
 encode({discard, Path}) ->
-    encode_msg(<<"discard!">>, Path);
+    yes_msg:encode(<<"discard!">>, Path);
 
 encode({error, parse_error}) ->
-    encode_msg(<<"error">>, <<"parse error">>).
+    yes_msg:encode(<<"error">>, <<"parse error">>).
 
 decode(Bin) ->
-    decode_msg(Bin, [], <<>>).
+    decode_msg(Bin).
 
 %% == Internal
 
-encode_msg(BinOp, Payload) ->
-    BinPayloadSize = integer_to_binary(byte_size(Payload)),
-    <<BinOp/binary, " ", BinPayloadSize/binary, "\r\n",
-      Payload/binary, "\r\n">>.
-
-decode_msg(<<>>, MsgAcc, Remaining) ->
+decode_msg(Bin) ->
     F = fun({Op, Payload}) ->
         interp(Op, Payload)
     end,
-    Msgs = lists:map(F, lists:reverse(MsgAcc)),
-    {Msgs, Remaining};
-
-decode_msg(Bin, MsgAcc, _) ->
-    case decode_msg_flip(Bin) of
-        remaining ->
-            decode_msg(<<>>, MsgAcc, Bin);
-        {Op, PayloadSize, NewBin} ->
-            case decode_msg_flop(NewBin, PayloadSize) of
-                remaining ->
-                    decode_msg(<<>>, MsgAcc, Bin);
-                {Payload, Rest} ->
-                    Msg = {Op, Payload},
-                    decode_msg(Rest, [Msg | MsgAcc], <<>>)
-            end
+    case yes_msg:decode(Bin) of
+        {ok, BinMsgs, Rem} ->
+            Msgs = lists:map(F, lists:reverse(BinMsgs)),
+            {Msgs, Rem};
+        {error, Error} ->
+            {error, Error}
     end.
-
-decode_msg_flip(Bin) ->
-    case binary:match(Bin, ?NL) of
-        nomatch ->
-            remaining;
-        {Pos, 2} ->
-            BinSize = byte_size(Bin),
-            FlipBin = binary:part(Bin, {0, Pos}),
-            {Op, PayloadSize} = extract_flip(FlipBin),
-            Rest = binary:part(Bin, {Pos + 2, BinSize - (Pos + 2)}),
-            {Op, PayloadSize, Rest}
-    end.
-
-decode_msg_flop(Bin, PayloadSize) ->
-    BinSize = byte_size(Bin),
-    case BinSize >= (PayloadSize + 2) of
-        true ->
-            Payload = binary:part(Bin, {0, PayloadSize}),
-            Rest = binary:part(Bin, {PayloadSize + 2, BinSize - (PayloadSize + 2)}),
-            {Payload, Rest};
-        _ ->
-            remaining
-    end.
-
-extract_flip(Bin) ->
-    [Op, BinPayloadSize] = binary:split(Bin, <<" ">>),
-    {Op, binary_to_integer(BinPayloadSize)}.
 
 interp(<<"path-symbols">>, Payload) ->
-    {symbols, jsx:decode(Payload, [return_maps])};
+    [Path, Json] = binary:split(Payload, <<"\r\n">>),
+    {symbols, Path, jsx:decode(Json, [return_maps])};
 
 interp(<<"path-symbols?">>, Payload) ->
     {symbolsQ, Payload};
@@ -126,34 +84,35 @@ interp(<<"watch!">>, Payload) ->
 -include_lib("eunit/include/eunit.hrl").
 
 encode_test() ->
-    E = <<"path-symbols 43\r\n{\"functions\":[{\"line\":15,\"name\":\"getfun\"}]}\r\n">>,
+    E = <<"path-symbols;53\r\n/tmp/foo\r\n{\"functions\":[{\"line\":15,\"name\":\"getfun\"}]}\r\n">>,
+    Path = <<"/tmp/foo">>,
     Symbols = #{<<"functions">> => [
         #{<<"name">> => <<"getfun">>,
           <<"line">> => 15}
     ]},
-    R = whatels_msg:encode({symbols, Symbols}),
+    {ok, R} = whatels_msg:encode({symbols, Path, Symbols}),
     ?assertEqual(E, R).
 
 decode_test() ->
-    E = {symbols, #{<<"functions">> => [
+    E = {symbols, <<"/tmp/foo">>, #{<<"functions">> => [
                     #{<<"name">> => <<"getfun">>,
                     <<"line">> => 15}]}},
-    Bin = <<"path-symbols 43\r\n{\"functions\":[{\"line\":15,\"name\":\"getfun\"}]}\r\n">>,
+    Bin = <<"path-symbols;53\r\n/tmp/foo\r\n{\"functions\":[{\"line\":15,\"name\":\"getfun\"}]}\r\n">>,
     {[R], _} = whatels_msg:decode(Bin),
     ?assertEqual(E, R).
 
 decode_symbols_test() ->
     Source = <<"apps/whatels/src/whatels_handler.erl">>,
     SourceSizeBin = integer_to_binary(byte_size(Source)),
-    Bin = <<"path-symbols? ", SourceSizeBin/binary, "\r\n", Source/binary, "\r\n">>,
+    Bin = <<"path-symbols?;", SourceSizeBin/binary, "\r\n", Source/binary, "\r\n">>,
     E = {symbolsQ, Source},
     {[R], _} = whatels_msg:decode(Bin),
     ?assertEqual(E, R).
 
 decode_symbols_leftovers_test() ->
-    Bin = <<"path-symbols? 0\r\n">>,
+    Bin = <<"path-symbols?;0\r\n">>,
     {Ls, Rem} = whatels_msg:decode(Bin),
     ?assertEqual(Ls, []),
-    ?assertEqual(Rem, <<"path-symbols? 0\r\n">>).
+    ?assertEqual(Rem, <<"path-symbols?;0\r\n">>).
 
 -endif.
